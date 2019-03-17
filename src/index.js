4ba7075f9b98f8bd7a58489c76ee3c306cccd2a6
newalex1985 <newalex1985@tut.by>
2019-03-17 23:08:53 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+
+  if (preferences.length == 0) {
+    return 0;
+  }
+
+  var exludedNum = [];
+  var count = 0;
+
+  for (var i = 0; i < preferences.length; i++) {
+
+    if (exludedNum.indexOf(preferences[i]) != -1) {
+      continue;
+    }
+
+    if (preferences[i] == (i + 1)) {
+      exludedNum.push(preferences[i]);
+      continue;
+    }
+
+    if ((preferences[preferences[preferences[i] - 1] - 1] - 1) == i) {
+      count++;
+      exludedNum.push(preferences[i]);
+      exludedNum.push(preferences[preferences[i] - 1]);
+      exludedNum.push(preferences[preferences[preferences[i] - 1] - 1]);
+    }
+  }
+  return count;
+}
